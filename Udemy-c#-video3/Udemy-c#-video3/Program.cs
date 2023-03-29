8a23b81e8ecbf04cbede64991733c17e38a07287
Jose Luis Vallejos <joseluisvallejos1995@gmail.com>
2023-03-29 18:14:19 -0300

    feat: :sparkles: Variables e interpolacion

--- Udemy-c#-video3/Udemy-c#-video3/Program.cs
+++ Udemy-c#-video3/Udemy-c#-video3/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿//Variables, se declaran antes de utilizarlas.
+
+string nombre = "Jose Luis";
+int edad =  27;
+
+//Esto es la interpolacion, pero tambien existe la concatenacion clasica con el signo +
+Console.WriteLine($"Me llamo {nombre} y tengo {edad} anios!");
+Console.WriteLine($"Tengo {edad} anios y me llamo {nombre}");
+
+int edad_siguiente = edad + 1;
+Console.WriteLine($"Tedre {edad_siguiente} anios, el proximo anio y me llamo {nombre}");
