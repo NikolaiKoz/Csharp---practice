c69f3fb965ca9900154838d7e6bd1869c2adc3b0
Jose Luis Vallejos <joseluisvallejos1995@gmail.com>
2023-05-10 22:34:00 -0300

    feat: :sparkles: Ejercicios.

--- Udemy-c#-video3/Udemy-c#-video3/Program.cs
+++ Udemy-c#-video3/Udemy-c#-video3/Program.cs
@@ -9,3 +9,4 @@ Console.WriteLine($"Tengo {edad} anios y me llamo {nombre}");
 
 int edad_siguiente = edad + 1;
 Console.WriteLine($"Tedre {edad_siguiente} anios, el proximo anio y me llamo {nombre}");
+
